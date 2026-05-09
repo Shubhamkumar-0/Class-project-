e3945405ef9633fa05d49c36609cb4e96e46e985
Shubham Kumar <shubhamkumarerror1@gmail.com>
2026-05-09 09:28:25 +0530

    backend connectend

--- src/api/classAPI.js
+++ src/api/classAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE = '/api/classes';
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://class-project-rbe0.onrender.com';
 const API_BASE = `${BASE_URL}/api/classes`;
 
 export const classAPI = {
